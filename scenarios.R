bf567b86c2276a14be8037431f162a2a4c76de47
Kim Larsen <thirdlovechangethisname@MacBook-Air-6.local>
2018-07-06 09:39:46 -0700

    fixing some naming conventions

--- scenarios.R
+++ scenarios.R
@@ -12,8 +12,6 @@ ggsave(growth1[[3]], file="baseline_revenue.png", device = "png", dpi=72, width=
 ggsave(growth1[[4]], file="baseline_cac.png", device = "png", dpi=72, width=9, height=6)
 ggsave(growth1[[6]], file="baseline_churn_acq_ratio.png", device = "png", dpi=72, width=9, height=6)
 
-growth1[[7]]
-
 ### Scenario 2: Add more marketing in year 3+
 growth2 <- run_scenario(marketing_elasticity=0.30, engagement=1, price=60, n=60, initial_marketing=500000/12,
                            marketing_allocation=0.2, base=1000, survival_rate=0.9, gm=0.4, initial_dropoff=0.15, 
