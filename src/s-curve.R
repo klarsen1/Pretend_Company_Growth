455372df8315921a941d33a83bb02ef4261bb314
Kim Larsen <thirdlovechangethisname@MacBook-Air-6.thirdlove.int>
2018-07-10 12:35:13 -0700

    change s curve graph

--- src/s-curve.R
+++ src/s-curve.R
@@ -6,6 +6,7 @@ s <- function(x) {
 
 p <- ggplot(data.frame(x = c(-4, 4)), aes(x = x)) +
   stat_function(fun = s, size=1) + xlab("Time") + ylab("Growth") + 
-  theme(axis.ticks=element_blank(), axis.text.x=element_blank(), axis.text.y=element_blank())
+  theme(axis.ticks=element_blank(), axis.text.x=element_blank(), axis.text.y=element_blank()) + 
+  geom_vline(xintercept = 2, linetype=3) +  geom_vline(xintercept = -2, linetype=3)
 
 ggsave(p, file="s.png", device = "png", dpi=72, width=9, height=6)
