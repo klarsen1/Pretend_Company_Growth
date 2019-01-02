2c05e7c8ee1221ad2bdbb5dc0e29330cc15ea2d2
Kim Larsen <kim.larsen@RTD.local>
2019-01-01 22:13:36 -0800

    expanding horizon

--- marketing_versus_engagement.R
+++ marketing_versus_engagement.R
@@ -16,12 +16,13 @@ ggsave(baseline[[6]], file="baseline_churn_acq_ratio.png", device = "png", dpi=7
 
 ### Optimize
 
+Opt_Year <- 7
 Results <- list()
 s <- 1
-for (a in seq(.10, .20, by=.05)){
-  for (pe in seq(2.5, 4, by=0.5)){
-     for (me in seq(0.20, 0.30, by=0.05)){
-        for (i in seq(a-.09, a, by=0.01)){
+for (a in seq(.20, .20, by=.05)){
+  for (pe in seq(2, 3.5, by=0.5)){
+     for (me in seq(0.20, 0.35, by=0.05)){
+        for (i in seq(0.05, a, by=0.01)){
            print(paste0("Scenario: ", s))
            print(paste0("Marketing Elasticity: ", me))
            print(paste0("Price Elasticity: ", pe))
@@ -37,12 +38,12 @@ for (a in seq(.10, .20, by=.05)){
            print(paste0("Engagement Scalar: ", engagement_scalar))
            print(paste0("Marketing Scalar: ", marketing_scalar))
            cat("\n")
-           scenario <- run_scenario(marketing_elasticity=me, engagement=1, price=60, n=60, initial_marketing=500000/12,
+           scenario <- run_scenario(marketing_elasticity=me, engagement=1, price=60, n=Opt_Year*12, initial_marketing=500000/12,
                                      marketing_allocation=a, base=1000, survival_rate=0.9, gm=0.4, initial_dropoff=0.15, 
                                      maxlim_revenue=1400, maxlim_cac=500, 
                                      price_boost=price_scalar, marketing_boost=marketing_scalar, engagement_boost=engagement_scalar,
                                      boost_year=4)
-           Results[[s]] <- filter(scenario[[2]], Year==5) %>% 
+           Results[[s]] <- filter(scenario[[2]], Year==Opt_Year) %>% 
              mutate(Marketing_Elasticity=me, 
                     Price_Elasticity=pe,
                     Current_Allocation=a,
@@ -62,17 +63,19 @@ Scenarios <- data.frame(rbindlist(Results)) %>%
   group_by(Current_Allocation, Marketing_Elasticity, Price_Elasticity) %>%
   filter(as.numeric(row_number())==1) %>%
   mutate(Scenario=paste0("MA: ", round(100*Current_Allocation,2), "% -- ME: ", round(100*Marketing_Elasticity,2), "%"), 
+         ME=paste0("ME: ", Marketing_Elasticity),
          Incentive_Allocation=Discount/.1)
          
-graph <- ggplot(Scenarios, aes(x=Price_Elasticity, y=Discount)) +
+graph1 <- ggplot(Scenarios, aes(x=Price_Elasticity, y=Discount)) +
   geom_bar(stat="identity") +
-  facet_wrap(Scenario ~ ., scales="fixed") +
+  facet_wrap(ME ~ ., scales="fixed") +
   xlab("Incentive Elasticity") +
   ylab("% Marketing Reallocated to Incentives") +
-  scale_y_continuous(limits=c(0,.1), breaks=seq(0, .1, by=.025), labels = scales::percent) + 
-  scale_x_continuous(breaks=seq(2.5, 4, by=0.5))
-graph  
+  scale_y_continuous(breaks=seq(0, .15, by=.05), labels = scales::percent) + 
+  scale_x_continuous(breaks=seq(2, 3.5, by=0.5))
 
+graph1  
+#ggsave(graph1, file="incentives_versus_marketing_5years.png", device = "png", dpi=72, width=9, height=6)
 
 d <- data.frame(matrix(nrow=5, ncol=2))
 
